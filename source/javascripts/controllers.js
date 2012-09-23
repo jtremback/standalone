d2b0a24d2eceee1beb25d4cd6fb6ee0786a41f57
Jehan Tremback <jehan.tremback@gmail.com>
2012-09-23 09:13:55 -0700

    fresh start

--- source/javascripts/controllers.js
+++ source/javascripts/controllers.js
@@ -1,7 +1,12 @@
-function CommentListCtrl($scope, $http) {
-  alert("hello");
-  $http.get('data.json').success(function(data) {
-    $scope.tree = data;
-    alert("goodbye")
-  });
-}
+// function CommentListCtrl($scope, $http) {
+//   alert("hello");
+//   $http.get('data.json').success(function(data) {
+//     $scope.tree = data;
+//     alert("goodbye")
+//   });
+// }
+
+angular.module("myApp", []).
+controller("TreeController", ['$scope', function($scope) {
+$scope.tree = [{name: "Node", snorp: "crap", nodes: [ {name: "Nope", snorp: "crop", nodes: [{name:"Nape", snorp: "crip"}]} ]}];
+}])
