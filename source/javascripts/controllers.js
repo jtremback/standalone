fdae40008ea94391f1071880374f24846a199229
Jehan Tremback <jehan.tremback@gmail.com>
2012-09-23 17:58:57 -0700

    simple nesting works, no json

--- source/javascripts/controllers.js
+++ source/javascripts/controllers.js
@@ -1,12 +1,7 @@
-// function CommentListCtrl($scope, $http) {
-//   alert("hello");
-//   $http.get('data.json').success(function(data) {
-//     $scope.tree = data;
-//     alert("goodbye")
-//   });
-// }
+'use strict';
 
-angular.module("myApp", []).
-controller("TreeController", ['$scope', function($scope) {
-$scope.tree = [{name: "Node", snorp: "crap", nodes: [ {name: "Nope", snorp: "crop", nodes: [{name:"Nape", snorp: "crip"}]} ]}];
-}])
+/* Controllers */
+
+function PhoneListCtrl($scope) {
+  $scope.tree = [{name: "Node", snorp: "crap", nodes: [ {name: "Nope", snorp: "crop", nodes: [{name:"Nape", snorp: "crip", nodes: []}]} ]}];
+}
