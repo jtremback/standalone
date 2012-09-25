2ea4f1c5642eb771f40ed8510b854ffadf0eca14
Jehan Tremback <jehan.tremback@gmail.com>
2012-09-25 15:30:28 -0700

    lots of refactor, dub quotes

---
+++ build-a1faf23/javascripts/filters.js
@@ -0,0 +1,40 @@
+/*
+Truncate Filter
+@Param text
+@Param length, default is 10
+@Param end, default is "..."
+@return string
+*/
+
+
+
+angular.module("annoFilters", []).filter("truncate", function() {
+  return function(text, length, end) {
+    if (isNaN(length)) {
+      length = text.length;
+    }
+    if (end === undefined) {
+      end = "...";
+    }
+    if (text.length <= length || text.length - end.length <= length) {
+      return text;
+    } else {
+      return String(text).substring(0, length - end.length) + end;
+    }
+  };
+});
+
+/*
+Usage
+
+var myText = "This is an example.";
+
+{{myText|Truncate}}
+{{myText|Truncate:5}}
+{{myText|Truncate:25:" ->"}}
+Output
+"This is..."
+"Th..."
+"This is an e ->"
+*/
+;
